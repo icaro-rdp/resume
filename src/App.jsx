cad99341703a0d48f4c1d85043ac125f00106a44
Icaro Re Depaolini <igher97@gmail.com>
2022-10-13 22:35:02 +0200

    first changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 function App() {
   return (
-    <div className="bg-gray-700 h-screen">
-      <h1 className="text-4xl text-white font-bold underline">Hello world!</h1>
+    <div className=" rounded-md  min-h-screen my-16 bg-gray-100 max-w-4xl mx-auto ">
+      <div className="grid gap-2 grid-cols-3 w-full h-full">
+        <div className="bg-slate-600 text-white col-span-1">dadsa</div>
+        <div className="bg-gray-100 text-slate-600 col-span-2">
+          <h1>Re Depaolini Icaro</h1>
+          <h2>Frontend Developer</h2>
+          <hr />
+        </div>
+      </div>
     </div>
   );
 }
