215232b94138bc657a74cf92d5aa7c497e40baf4
Icaro Re Depaolini <igher97@gmail.com>
2022-10-13 21:44:25 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-gray-700 h-screen">
+      <h1 className="text-4xl text-white font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
